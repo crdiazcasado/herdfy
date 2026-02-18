431e582d24040df08632450d53e548ebb21eb825
crdiazcasado <crdiazcasado@gmail.com>
2026-02-18 11:31:31 +0100

    Posthog integration

---
+++ app/providers.js
@@ -0,0 +1,16 @@
+'use client'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
+
+export function PHProvider({ children }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: 'identified_only',
+    })
+  }, [])
+
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+}
