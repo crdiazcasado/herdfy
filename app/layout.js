20d1e2f280d67e3a723a5cb69f258421788105ee
“crdiazcasado” <crdiazcasado@gmail.com>
2026-03-08 00:41:55 +0100

    correct some metadata and add icons

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,9 @@ export const metadata = {
   description: "Plataforma de creación de campañas sociales.",
   icons: {
     icon: '/icon.png',
-    apple: '/apple-touch-icon.png',
+    apple: [
+    { url: "/apple-touch-icon.png", sizes: "180x180"  }
+  ]
   },
 }
 
